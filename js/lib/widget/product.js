ddd1d536dd76eaae699b547d6abfbcb75aae40e2
anxsaiyuki <anxsaiyuki@gmail.com>
2014-04-25 10:19:07 +0800

    moved to module and php

--- js/lib/widget/product.js
+++ js/lib/widget/product.js
@@ -1,7 +1,7 @@
 define(['jquery'],function($){
 	return{
 		start:function(){
-			$.get( "product.html", function( data ) {
+			$.get( "module/product.php", function( data ) {
 			  $( "#product_container" ).html( data );
 			  console.log( "Product Load was Performed." );
 			});
---
+++ module/product.php
@@ -0,0 +1,272 @@
+<div class="col-md-2 col-xs-2 product">
+	<div class="col-md-11 col-md-offset-1 col-xs-11 col-xs-offset-1 name">
+		Name
+	</div>
+	<div class="col-md-11 col-md-offset-1 col-xs-11 col-xs-offset-1 image">
+		Image
+	</div>
+	<div class="col-md-11 col-md-offset-1 col-xs-11 col-xs-offset-1 description">
+		Description
+	</div>
+	<div class="col-md-7 col-md-offset-1 col-xs-7 col-xs-offset-1 price">
+		Price
+	</div>
+	<div class="col-md-4 button col-xs-4">
+		Buy
+	</div>
+</div>
+<div class="col-md-2 col-xs-2 product">
+	<div class="col-md-11 col-md-offset-1 col-xs-11 col-xs-offset-1 name">
+		Name
+	</div>
+	<div class="col-md-11 col-md-offset-1 col-xs-11 col-xs-offset-1 image">
+		Image
+	</div>
+	<div class="col-md-11 col-md-offset-1 col-xs-11 col-xs-offset-1 description">
+		Description
+	</div>
+	<div class="col-md-7 col-md-offset-1 col-xs-7 col-xs-offset-1 price">
+		Price
+	</div>
+	<div class="col-md-4 button col-xs-4">
+		Buy
+	</div>
+</div>
+<div class="col-md-2 col-xs-2 product">
+	<div class="col-md-11 col-md-offset-1 col-xs-11 col-xs-offset-1 name">
+		Name
+	</div>
+	<div class="col-md-11 col-md-offset-1 col-xs-11 col-xs-offset-1 image">
+		Image
+	</div>
+	<div class="col-md-11 col-md-offset-1 col-xs-11 col-xs-offset-1 description">
+		Description
+	</div>
+	<div class="col-md-7 col-md-offset-1 col-xs-7 col-xs-offset-1 price">
+		Price
+	</div>
+	<div class="col-md-4 button col-xs-4">
+		Buy
+	</div>
+</div>
+<div class="col-md-2 col-xs-2 product">
+	<div class="col-md-11 col-md-offset-1 col-xs-11 col-xs-offset-1 name">
+		Name
+	</div>
+	<div class="col-md-11 col-md-offset-1 col-xs-11 col-xs-offset-1 image">
+		Image
+	</div>
+	<div class="col-md-11 col-md-offset-1 col-xs-11 col-xs-offset-1 description">
+		Description
+	</div>
+	<div class="col-md-7 col-md-offset-1 col-xs-7 col-xs-offset-1 price">
+		Price
+	</div>
+	<div class="col-md-4 button col-xs-4">
+		Buy
+	</div>
+</div>
+<div class="col-md-2 col-xs-2 product">
+	<div class="col-md-11 col-md-offset-1 col-xs-11 col-xs-offset-1 name">
+		Name
+	</div>
+	<div class="col-md-11 col-md-offset-1 col-xs-11 col-xs-offset-1 image">
+		Image
+	</div>
+	<div class="col-md-11 col-md-offset-1 col-xs-11 col-xs-offset-1 description">
+		Description
+	</div>
+	<div class="col-md-7 col-md-offset-1 col-xs-7 col-xs-offset-1 price">
+		Price
+	</div>
+	<div class="col-md-4 button col-xs-4">
+		Buy
+	</div>
+</div>
+<div class="col-md-2 col-xs-2 product">
+	<div class="col-md-11 col-md-offset-1 col-xs-11 col-xs-offset-1 name">
+		Name
+	</div>
+	<div class="col-md-11 col-md-offset-1 col-xs-11 col-xs-offset-1 image">
+		Image
+	</div>
+	<div class="col-md-11 col-md-offset-1 col-xs-11 col-xs-offset-1 description">
+		Description
+	</div>
+	<div class="col-md-7 col-md-offset-1 col-xs-7 col-xs-offset-1 price">
+		Price
+	</div>
+	<div class="col-md-4 button col-xs-4">
+		Buy
+	</div>
+</div>
+<div class="col-md-2 col-xs-2 product">
+	<div class="col-md-11 col-md-offset-1 col-xs-11 col-xs-offset-1 name">
+		Name
+	</div>
+	<div class="col-md-11 col-md-offset-1 col-xs-11 col-xs-offset-1 image">
+		Image
+	</div>
+	<div class="col-md-11 col-md-offset-1 col-xs-11 col-xs-offset-1 description">
+		Description
+	</div>
+	<div class="col-md-7 col-md-offset-1 col-xs-7 col-xs-offset-1 price">
+		Price
+	</div>
+	<div class="col-md-4 button col-xs-4">
+		Buy
+	</div>
+</div>
+<div class="col-md-2 col-xs-2 product">
+	<div class="col-md-11 col-md-offset-1 col-xs-11 col-xs-offset-1 name">
+		Name
+	</div>
+	<div class="col-md-11 col-md-offset-1 col-xs-11 col-xs-offset-1 image">
+		Image
+	</div>
+	<div class="col-md-11 col-md-offset-1 col-xs-11 col-xs-offset-1 description">
+		Description
+	</div>
+	<div class="col-md-7 col-md-offset-1 col-xs-7 col-xs-offset-1 price">
+		Price
+	</div>
+	<div class="col-md-4 button col-xs-4">
+		Buy
+	</div>
+</div>
+<div class="col-md-2 col-xs-2 product">
+	<div class="col-md-11 col-md-offset-1 col-xs-11 col-xs-offset-1 name">
+		Name
+	</div>
+	<div class="col-md-11 col-md-offset-1 col-xs-11 col-xs-offset-1 image">
+		Image
+	</div>
+	<div class="col-md-11 col-md-offset-1 col-xs-11 col-xs-offset-1 description">
+		Description
+	</div>
+	<div class="col-md-7 col-md-offset-1 col-xs-7 col-xs-offset-1 price">
+		Price
+	</div>
+	<div class="col-md-4 button col-xs-4">
+		Buy
+	</div>
+</div>
+<div class="col-md-2 col-xs-2 product">
+	<div class="col-md-11 col-md-offset-1 col-xs-11 col-xs-offset-1 name">
+		Name
+	</div>
+	<div class="col-md-11 col-md-offset-1 col-xs-11 col-xs-offset-1 image">
+		Image
+	</div>
+	<div class="col-md-11 col-md-offset-1 col-xs-11 col-xs-offset-1 description">
+		Description
+	</div>
+	<div class="col-md-7 col-md-offset-1 col-xs-7 col-xs-offset-1 price">
+		Price
+	</div>
+	<div class="col-md-4 button col-xs-4">
+		Buy
+	</div>
+</div>
+<div class="col-md-2 col-xs-2 product">
+	<div class="col-md-11 col-md-offset-1 col-xs-11 col-xs-offset-1 name">
+		Name
+	</div>
+	<div class="col-md-11 col-md-offset-1 col-xs-11 col-xs-offset-1 image">
+		Image
+	</div>
+	<div class="col-md-11 col-md-offset-1 col-xs-11 col-xs-offset-1 description">
+		Description
+	</div>
+	<div class="col-md-7 col-md-offset-1 col-xs-7 col-xs-offset-1 price">
+		Price
+	</div>
+	<div class="col-md-4 button col-xs-4">
+		Buy
+	</div>
+</div>
+<div class="col-md-2 col-xs-2 product">
+	<div class="col-md-11 col-md-offset-1 col-xs-11 col-xs-offset-1 name">
+		Name
+	</div>
+	<div class="col-md-11 col-md-offset-1 col-xs-11 col-xs-offset-1 image">
+		Image
+	</div>
+	<div class="col-md-11 col-md-offset-1 col-xs-11 col-xs-offset-1 description">
+		Description
+	</div>
+	<div class="col-md-7 col-md-offset-1 col-xs-7 col-xs-offset-1 price">
+		Price
+	</div>
+	<div class="col-md-4 button col-xs-4">
+		Buy
+	</div>
+</div>
+<div class="col-md-2 col-xs-2 product">
+	<div class="col-md-11 col-md-offset-1 col-xs-11 col-xs-offset-1 name">
+		Name
+	</div>
+	<div class="col-md-11 col-md-offset-1 col-xs-11 col-xs-offset-1 image">
+		Image
+	</div>
+	<div class="col-md-11 col-md-offset-1 col-xs-11 col-xs-offset-1 description">
+		Description
+	</div>
+	<div class="col-md-7 col-md-offset-1 col-xs-7 col-xs-offset-1 price">
+		Price
+	</div>
+	<div class="col-md-4 button col-xs-4">
+		Buy
+	</div>
+</div>
+<div class="col-md-2 col-xs-2 product">
+	<div class="col-md-11 col-md-offset-1 col-xs-11 col-xs-offset-1 name">
+		Name
+	</div>
+	<div class="col-md-11 col-md-offset-1 col-xs-11 col-xs-offset-1 image">
+		Image
+	</div>
+	<div class="col-md-11 col-md-offset-1 col-xs-11 col-xs-offset-1 description">
+		Description
+	</div>
+	<div class="col-md-7 col-md-offset-1 col-xs-7 col-xs-offset-1 price">
+		Price
+	</div>
+	<div class="col-md-4 button col-xs-4">
+		Buy
+	</div>
+</div>
+<div class="col-md-2 col-xs-2 product">
+	<div class="col-md-11 col-md-offset-1 col-xs-11 col-xs-offset-1 name">
+		Name
+	</div>
+	<div class="col-md-11 col-md-offset-1 col-xs-11 col-xs-offset-1 image">
+		Image
+	</div>
+	<div class="col-md-11 col-md-offset-1 col-xs-11 col-xs-offset-1 description">
+		Description
+	</div>
+	<div class="col-md-7 col-md-offset-1 col-xs-7 col-xs-offset-1 price">
+		Price
+	</div>
+	<div class="col-md-4 button col-xs-4">
+		Buy
+	</div>
+</div>
+<div class="col-md-2 col-xs-2 product">
+	<div class="col-md-11 col-md-offset-1 col-xs-11 col-xs-offset-1 name">
+		Name
+	</div>
+	<div class="col-md-11 col-md-offset-1 col-xs-11 col-xs-offset-1 image">
+		Image
+	</div>
+	<div class="col-md-11 col-md-offset-1 col-xs-11 col-xs-offset-1 description">
+		Description
+	</div>
+	<div class="col-md-7 col-md-offset-1 col-xs-7 col-xs-offset-1 price">
+		Price
+	</div>
+	<div class="col-md-4 button col-xs-4">
+		Buy
+	</div>
+</div>
