8be5cb2440abef8ea905cdb1e29b5c2255c7f1bf
anxsaiyuki <anxsaiyuki@gmail.com>
2014-04-25 13:05:20 +0800

    item_page image link fix2

--- module/product.php
+++ module/product.php
@@ -191,7 +191,7 @@
 	</div>
 	<a href="product.html"><div class="col-md-11 col-md-offset-1 col-xs-11 col-xs-offset-1 image">
 		Image
-	</div>
+	</div></a>
 	<div class="col-md-11 col-md-offset-1 col-xs-11 col-xs-offset-1 description">
 		Description
 	</div>
@@ -206,7 +206,7 @@
 	<div class="col-md-11 col-md-offset-1 col-xs-11 col-xs-offset-1 name">
 		Name
 	</div>
-	<a href="product.html"><div class="col-md-11 col-md-offset-1 col-xs-11 col-xs-offset-1 image"></a>
+	<a href="product.html"><div class="col-md-11 col-md-offset-1 col-xs-11 col-xs-offset-1 image">
 		Image
 	</div></a>
 	<div class="col-md-11 col-md-offset-1 col-xs-11 col-xs-offset-1 description">
